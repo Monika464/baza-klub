b736d51e0f6624faeffb7173094ddff09761172a
Monika464 <74193434+Monika464@users.noreply.github.com>
2023-06-28 23:25:48 +0200

    Create adminpanel.js
    makingPayment

---
+++ adminpanel.js
@@ -0,0 +1,25 @@
+const { getData,aDate,findingIndex,sortingFirst,findAdate } = useSortDates('trainingDates','created_at','datesSet',100  );
+
+const makingPayment = async() =>{
+await usersFromBase?.map(async(el,ind)=>{
+if(selectedUser.value === el.id){
+const   date1 = await el.due ;
+setDateQuest(date1)
+await getData();
+await sortingFirst();
+await findingIndex(date1);
+await findAdate(4);
+}
+} )
+}
+console.log('aDate',aDate)
+     
+
+   
+  return(
+   <>
+      <p>{aDate.toDate().ToLocaleDateString()}</p>
+  
+ 	 </>);
+  
+  
